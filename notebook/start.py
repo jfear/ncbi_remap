e4e893c06d4eb3ebb92fb04fb386da9780cea1db
Justin Fear <justin.m.fear@gmail.com>
2016-09-02 08:39:34 -0400

    Adds project specific lib

--- notebook/start.py
+++ notebook/start.py
@@ -1,3 +1,5 @@
+# Load useful extensions
+
 # Activate the autoreload extension for easy reloading of external packages
 %reload_ext autoreload
 %autoreload 1
@@ -5,3 +7,12 @@
 # Trun on the water mark
 %reload_ext watermark
 %watermark -u -d -v
+
+# Load ipycache extension
+%reload_ext ipycache
+from ipycache import CacheMagics
+CacheMagics.cachedir = '../cachedir'
+
+# Add project library to path
+import sys
+sys.path.insert(0, '../../lib/python')
