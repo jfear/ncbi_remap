449afbbd888c6e0e20281be33d24ad9f12bd14f6
Justin Fear <justin.fear@nih.gov>
2016-09-02 08:36:29 -0400

    adds cachedir to start.py

--- notebook/start.py
+++ notebook/start.py
@@ -8,8 +8,13 @@ each notebook.
 
 # Activate the autoreload extension for easy reloading of external packages
 %reload_ext autoreload
-autoreload 1
+%autoreload 1
+
+# Set up cashdir
+from ipycache import CacheMagics
+CacheMagics.cachedir = '../cachedir'
 
 # Trun on the water mark
 %reload_ext watermark
 %watermark -u -d -v
+
