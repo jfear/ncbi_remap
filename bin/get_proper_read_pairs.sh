d4fe3fb3a27285d1aede6c7bc6b4cea3b173d74e
Justin Fear <justin.m.fear@gmail.com>
2017-02-03 14:23:00 -0500

    adds a script to make sure fastq's have proper read pairs.

---
+++ bin/get_proper_read_pairs.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Inputs
+FNAME1='../output/prealignment/raw/SRX1065034/SRR2069724/SRR2069724_1.fastq.gz'
+FNAME2='../output/prealignment/raw/SRX1065034/SRR2069724/SRR2069724_2.fastq.gz'
+
+# Outputs
+ONAME1=${FNAME1/fastq.gz/clean.fastq.gz}
+ONAME2=${FNAME2/fastq.gz/clean.fastq.gz}
+
+# Decompress gz files
+DECOMP1=$(mktemp -p /lscratch/$SLURM_JOBID)
+DECOMP2=$(mktemp -p /lscratch/$SLURM_JOBID)
+
+gunzip -c $FNAME1 | paste - - - - | awk 'BEGIN{FS=" "; OFS="\t"}{print $1"\t"$0}' | sort -k1,1n > $DECOMP1
+gunzip -c $FNAME2 | paste - - - - | awk 'BEGIN{FS=" "; OFS="\t"}{print $1"\t"$0}' | sort -k1,1n > $DECOMP2
+
+# Merge data by first column
+MERGED=$(mktemp -p /lscratch/$SLURM_JOBID)
+join -t $'\t' -j 1 -o 1.2,1.3,1.4,1.5,2.2,2.3,2.4,2.5 $DECOMP1 $DECOMP2 > $MERGED
+
+# Split back out
+OUT1=$(mktemp -p /lscratch/$SLURM_JOBID)
+OUT2=$(mktemp -p /lscratch/$SLURM_JOBID)
+
+awk -v out1=$OUT1 -v out2=$OUT2 'BEGIN{FS="\t"}{for (i=1; i<=4; i++){print $i >> out1}; for(i=5;i<=8; i++){print $i >> out2}}' $MERGED
+
+# Zip it back up
+gzip -c $OUT1 > $ONAME1
+gzip -c $OUT2 > $ONAME2
+
+# Clean up
+rm $DECOMP1 $DECOMP2
+rm $MERGED
+rm $OUT1 $OUT2
