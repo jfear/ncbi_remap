1b70290f428e10beb96734793fc39848edef63f3
Justin Fear <justin.m.fear@gmail.com>
2017-05-17 09:46:44 -0400

    Update

--- bin/start_db.sh
+++ bin/start_db.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+# Change ulimits
+ulimit -u 6400 -n 7000
+
 # Start mongo
-mongod --dbpath ../output/db --logpath ../output/logs/mongoDB.log --port 27022 --fork
+numactl --interleave=all mongod --dbpath ../output/db --logpath ../output/logs/mongoDB.log --port 27022 --fork
 hostname > ../output/.mongodb_host
