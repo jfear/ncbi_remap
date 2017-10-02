c1832a787bec3c9d59699475cf369a1fb98d5311
Justin Fear <justin.m.fear@gmail.com>
2017-10-01 20:07:23 -0400

    messes with ulimits to try to stop the db from crashing.

--- bin/start_db.sh
+++ bin/start_db.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Change ulimits
-ulimit -u 6400 -n 7000
+ulimit -u 4000
 
 # Start mongo
 numactl --interleave=all mongod --dbpath ../output/db --logpath ../output/logs/mongoDB.log --port 27022 --fork
