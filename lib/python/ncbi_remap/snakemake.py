1ca4fa3d14c83918c75dee3b145cb3952b0f6436
Justin Fear <justin.m.fear@gmail.com>
2017-10-18 15:23:08 -0400

    Switched out flags for files.

---
+++ lib/python/ncbi_remap/snakemake.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+"""Set of helpers for use with snakemake."""
+
+
+def wrapper_for(path):
+    URI = '../lcdb-wf/wrappers/wrappers'
+    return 'file:' + os.path.join(URI, path)
+
+
+def put_flag(fname, flag):
+    with open(fname, 'w') as fh:
+        fh.write(flag)
+
+
+def get_flag(fname):
+    with open(fname) as fh:
+        return fh.read().strip()
+
+
+def clean_types(dic):
+    if isinstance(dic, dict):
+        newDic = {}
+        for k, v in dic.items():
+            if isinstance(v, np.int64):
+                newDic[k] = int(v)
+            elif isinstance(v, np.float64):
+                newDic[k] = float(v)
+            elif v is np.nan:
+                pass
+            elif v is None:
+                pass
+            else:
+                newDic[k] = v
+        return newDic
+    elif isinstance(dic, list):
+        newLis = []
+        for row in dic:
+            newLis.append(clean_types(row))
+        return newLis
+
+
