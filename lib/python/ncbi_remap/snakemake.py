2b6f84dcbac7659d47eeac2b1dc660cb7db0a28f
Justin Fear <justin.m.fear@gmail.com>
2017-10-19 19:10:16 -0400

    Refactors workflow into custom wrappers.

--- lib/python/ncbi_remap/snakemake.py
+++ lib/python/ncbi_remap/snakemake.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python
 """Set of helpers for use with snakemake."""
-import os
-import numpy as np
 
 
 def wrapper_for(path):
-    URI = '../lcdb-wf/wrappers/wrappers'
-    return 'file:' + os.path.join(URI, path)
+    return 'file:' + path
 
 
 def put_flag(fname, flag):
@@ -17,27 +14,4 @@ def put_flag(fname, flag):
 def get_flag(fname):
     with open(fname) as fh:
         return fh.read().strip()
-
-
-def clean_types(dic):
-    if isinstance(dic, dict):
-        newDic = {}
-        for k, v in dic.items():
-            if isinstance(v, np.int64):
-                newDic[k] = int(v)
-            elif isinstance(v, np.float64):
-                newDic[k] = float(v)
-            elif v is np.nan:
-                pass
-            elif v is None:
-                pass
-            else:
-                newDic[k] = v
-        return newDic
-    elif isinstance(dic, list):
-        newLis = []
-        for row in dic:
-            newLis.append(clean_types(row))
-        return newLis
-
 
---
+++ prealn-wf/wrappers/fastq_dump/wrapper.py
@@ -0,0 +1,91 @@
+__author__ = "Justin Fear"
+__copyright__ = "Copyright 2016, Justin Fear"
+__email__ = "justin.fear@nih.gov"
+__license__ = "MIT"
+
+"""Example usage.
+
+rule fastq_dump:
+    output:
+        fq1=patterns['fastq']['r1'],
+        fq2=patterns['fastq']['r2'],
+        flag=patterns['layout'],
+        summary=patterns['fastq']['summary'],
+    log: patterns['fastq']['r1'] + '.log'
+    wrapper:
+        wrapper_for('wrappers/fastq_dump')
+
+"""
+
+import os
+import sys
+from snakemake.shell import shell
+
+sys.path.insert(0, '../lib/python')
+from ncbi_remap.fastq import check_fastq, md5sum, fastq_stats
+from ncbi_remap.snakemake import put_flag
+
+# Get TMPDIR
+TMPDIR = os.environ['TMPDIR']
+
+# Get current sample id
+sample = snakemake.wildcards.srr
+
+# Dump FASTQ to a TMPDIR
+shell("fastq-dump -O {TMPDIR} -M 0 --split-files {sample} {log}")
+
+# Summarize R1
+t1 = os.path.join(TMPDIR, sample + '_1.fastq')
+R1md5 = md5sum(t1)
+R1Libsize, R1avgLen = fastq_stats(t1)
+
+with open(t1, 'rb') as f_in:
+    with gzip.open(snakemake.output.fq1, 'wb') as f_out:
+        sh.copyfileobj(f_in, f_out)
+
+# Summarize R2 if it exists
+t2 = os.path.join(TMPDIR, sample + '_2.fastq')
+if check_fastq(t2):
+    # Get md5sum
+    R2md5 = md5sum(t2)
+    R2Libsize, R2avgLen = fastq_stats(t2)
+
+    with open(t2, 'rb') as f_in:
+        with gzip.open(snakemake.output.fq2, 'wb') as f_out:
+            sh.copyfileobj(f_in, f_out)
+else:
+    R2md5 = R2Libsize = R2avgLen = None
+    Path(snakemake.output.fq2).touch()
+
+# Save summaries
+df = pd.DataFrame([[R1md5, R1Libsize, R1avgLen, R2md5, R2Libsize, R2avgLen]],
+                  columns=['md5_R1', 'libsize_R1', 'avgLen_R1', 'md5_R2', 'libsize_R2', 'avgLen_R2'])
+
+df.to_csv(snakemake.output.summary, sep='\t', index=False)
+
+# Figure out flags
+if (R1Libsize > 1000) & (R2Libsize is None) & (R1avgLen > 10) & (R2avgLen is None):
+    # R2 does not exists so SE
+    put_flag(snakemake.output.flag, 'SE')
+
+elif (R1Libsize > 1000) & (R2Libsize > 1000) & (R1avgLen > 10) & (R2avgLen > 10):
+    if R1Libsize == R2Libsize:
+        # Both reads look good so PE
+        put_flag(snakemake.output.flag, 'PE')
+    else:
+        # There are an uneven number of reads between R1 and R2.
+        # Instead of messing with this, just consider SE and use R1.
+        put_flag(snakemake.output.flag, 'keep_R1')
+
+elif (R1Libsize > 1000) & (R1avgLen > 10):
+    # Only R1 looks ok, so SE
+    put_flag(snakemake.output.flag, 'keep_R1')
+
+elif (R2Libsize > 1000) & (R2avgLen > 10):
+    # Only R2 looks ok, consider single-end
+    put_flag(snakemake.output.flag, 'keep_R2')
+
+else:
+    # R1 and R2 look bad, flag as download bad
+    fname = os.path.join(os.path.dirname(snakemake.output.fastq), 'DOWNLOAD_BAD')
+    Path(fname).touch()
