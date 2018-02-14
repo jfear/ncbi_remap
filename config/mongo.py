520409cf5e7bed44b2c7005ce594b7aa8e2e8f06
Justin Fear <justin.m.fear@gmail.com>
2018-02-14 17:58:34 -0500

    Adds mongo default imports.

---
+++ config/mongo.py
@@ -0,0 +1,10 @@
+from pymongo import MongoClient
+try:
+    with open('../output/.mongodb_host', 'r') as fh:
+        host = fh.read().strip()
+except FileNotFoundError:
+    host = 'localhost'
+
+mongoClient = MongoClient(host=host, port=27022)
+db = mongoClient['sra']
+ncbi = db['ncbi']
