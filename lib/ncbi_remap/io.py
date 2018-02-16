63eaa14f82be00754f7dc2f8da699c808c3be40c
Justin Fear <justin.m.fear@gmail.com>
2018-02-16 16:47:25 -0500

    Starts an hdfstore class to better handle interacting with store.

--- lib/ncbi_remap/io.py
+++ lib/ncbi_remap/io.py
@@ -1,7 +1,181 @@
 #!/usr/bin/env python
 """ Set of helper scripts for file handling """
+from typing import Union
 import pandas as pd
 from IPython.display import display
+
+
+class HDFStore(object):
+    """A facade interface to pandas.HDFStore"""
+    def __init__(self, path, mode='a', srx_srr=None, *args, **kwargs):
+        self._store = pd.HDFStore(path, mode, *args, **kwargs)
+        if self._store.__contains__('ids'):
+            return
+
+        if srx_srr is not None:
+            self.initialize_store(srx_srr)
+        else:
+            print(
+                'To initialize the data store run '
+                'HDFStore.initialize_store or set `srx_srr` when '
+                'opening the data store.'
+            )
+
+    def __getitem__(self, key: str):
+        return self._store[key]
+
+    def __setitem__(self, key: str, value: Union[pd.Series, pd.DataFrame]):
+        self._store[key] = value
+
+    def __delitem__(self, key: str):
+        del self._store[key]
+
+    def close(self):
+        self._store.close()
+
+    @property
+    def root(self):
+        return self._store.root
+
+    def info(self):
+        return self._store.info()
+
+    @property
+    def ids(self):
+        return self._store['ids']
+
+    def initialize_store(self, srx_srr: pd.DataFrame):
+        """Run this once to create basic data store.
+
+        Parameters
+        ----------
+        srx_srr : pd.DataFrame
+            A dataframe with an index with SRX, SRR.
+
+        """
+        self._store['ids'] = srx_srr
+        self._store['prealn/queue'] = self.ids
+
+    def initialize_data_table(self, key, names, fill_bool=False):
+        """Create a new empty datatable.
+
+        Parameters
+        ----------
+        key : str
+            Data store key.
+        names : str or List[str]
+            Name or names of the columns. If a single name is given then a
+            pd.Series will be created, otherwise a pd.DataFrame.
+        fill_bool : bool
+            If true use the boolean value to fill missing instead of
+            default 'NA'.
+
+        """
+        if fill_bool:
+            fill = False
+        else:
+            fill = 'NA'
+
+        if isinstance(names, (list, tuple)) and len(names) == 1:
+            names = names[0]
+
+        if isinstance(names, str):
+            self._store[key] = pd.Series(name=names,
+                                         index=self.ids.index).fillna(fill)
+        else:
+            self._store[key] = pd.DataFrame(columns=names,
+                                            index=self.ids.index).fillna(fill)
+
+    def update_ids_from_db(self, current_srx_srr):
+        """
+
+        Parameters
+        ----------
+        current_srx_srr : pd.DataFrame
+            An updated set SRX, SRR ides. Note if an id is not in this list it
+            will be removed from the data store.
+
+        """
+        orig_srx_srr = self.ids
+
+        removed_ids = orig_srx_srr[
+            ~orig_srx_srr.index.isin(current_srx_srr.index)
+        ]
+
+        new_ids = current_srx_srr[
+            ~current_srx_srr.index.isin(orig_srx_srr.index)
+        ]
+
+        self._store['ids'] = current_srx_srr
+        for key in self._store.keys():
+            if key == '/ids':
+                continue
+
+            if 'complete' in key:
+                continue
+
+            self._append_new_ids(key, new_ids)
+            self._drop_removed_ids(key, removed_ids)
+
+    def _append_new_ids(self, key: str, new_srx_srr: pd.DataFrame, fill='NA'):
+        """Append new sra ids."""
+        if new_srx_srr.shape[0] == 0:
+            return
+
+        if not self._store.__contains__(key):
+            self._store[key] = new_srx_srr
+            return
+
+        orig = self._store[key]
+        new_dat = new_srx_srr.copy()
+
+        try:
+            for col in orig.columns:
+                new_dat[col] = fill
+        except AttributeError:
+            if orig.dtype.name == 'bool':
+                fill = False
+
+            new_dat[orig.name] = fill
+            new_dat = new_dat.loc[:, orig.name]
+
+        new = pd.concat([orig, new_dat])
+        self._store[key] = new[~new.index.duplicated('first')]
+
+    def _drop_removed_ids(self, key: str, removed_srx_srr: pd.DataFrame):
+        """Remove sra ids that are no longer present.
+
+        Somtime after running a database update SRA removes ids. I want remove
+        them from my work too.
+
+        """
+
+        if removed_srx_srr.shape[0] == 0:
+            return
+
+        orig = self._store[key]
+        curr_ids = orig[~orig.index.isin(removed_srx_srr.index)]
+        self._store[key] = curr_ids
+
+    def _tuple_to_frame(self, srx_srr: tuple):
+            return pd.DataFrame(index=pd.MultiIndex.from_tuples(
+                [srx_srr], names=['srx', 'srr']))
+
+    def pop(self, key: str,
+                  srx_srr: Union[tuple, pd.DataFrame]):
+
+        if isinstance(srx_srr, tuple):
+            srx_srr = self._tuple_to_frame(srx_srr)
+
+        self._drop_removed_ids(key, srx_srr)
+
+    def push(self, key: str,
+                   srx_srr: Union[tuple, pd.DataFrame]):
+
+        if isinstance(srx_srr, tuple):
+            srx_srr = self._tuple_to_frame(srx_srr)
+
+        self._append_new_ids(key, srx_srr)
 
 
 def build_index(store, key, columns=None):
