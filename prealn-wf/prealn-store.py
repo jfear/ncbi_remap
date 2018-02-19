c524d1a38daeadc9778e9cc4a68d749d63b72e32
Justin Fear <justin.m.fear@gmail.com>
2018-02-19 17:40:57 -0500

    working version of prealn-store.

--- prealn-wf/prealn-store.py
+++ prealn-wf/prealn-store.py
@@ -8,7 +8,6 @@ various flags and datasets.
 import sys
 from pathlib import Path
 import argparse
-from yaml import load
 
 import numpy as np
 import pandas as pd
@@ -23,6 +22,95 @@ from ncbi_remap.snakemake import get_patterns
 DATA_STORE = '../sra_new.h5'
 
 
+def get_options():
+    parser = argparse.ArgumentParser(
+        description='Pre-Alignment Queue',
+        usage="prealn_store.py <command> [args]")
+
+    subparsers = parser.add_subparsers(dest='command', help='Commands')
+
+    parser_init = subparsers.add_parser(
+        'init',
+        help='Initialize data store'
+    )
+    parser_init.add_argument(
+        '--host',
+        dest='host',
+        action='store',
+        default='localhost',
+        help='Host running mongo database [Optional].'
+    )
+    parser_init.add_argument(
+        '--port',
+        dest='port',
+        action='store',
+        type=int,
+        default=27022,
+        help='Mongo database port [Optional].'
+    )
+    parser_init.add_argument(
+        '-j',
+        dest='n_workers',
+        action='store',
+        default=2,
+        type=int,
+        help='Number of workers [default 2]',
+    )
+    parser_init.add_argument(
+        '--append',
+        dest='append',
+        action='store_true',
+        help='Download and append new ids to the data store.',
+    )
+
+    parser_queue = subparsers.add_parser('queue',
+                                         help='Interact with prealn/queue')
+    parser_queue.add_argument(
+        '-j',
+        dest='n_workers',
+        action='store',
+        default=2,
+        type=int,
+        help='Number of workers [default 2]',
+    )
+    parser_queue.add_argument(
+        '--update',
+        dest='queue_update',
+        action='store_true',
+        help='Run update on prealn/queue.',
+    )
+    parser_queue.add_argument(
+        '--print',
+        dest='queue_print',
+        action='store_true',
+        help='Print the prealn/queue',
+    )
+    parser_queue.add_argument(
+        '--abi',
+        dest='queue_abi',
+        action='store_true',
+        help='Check for ABI Solid data by fastq.',
+    )
+
+    parser_queue = subparsers.add_parser('data',
+                                         help='Interact with prealn/workflow')
+    parser_queue.add_argument(
+        '-j',
+        dest='n_workers',
+        action='store',
+        default=2,
+        type=int,
+        help='Number of workers [default 2]',
+    )
+    parser_queue.add_argument(
+        '--update',
+        dest='data_update',
+        action='store_true',
+        help='Run update on prealn/queue.',
+    )
+    return parser.parse_args()
+
+
 def get_keepers():
     return [
         patterns['fastq_screen'],
@@ -35,38 +123,6 @@ def get_keepers():
         patterns['bamtools_stats'],
         patterns['picard']['markduplicates']['metrics'],
     ]
-
-
-def get_options():
-    """Function to pull in command line arguments"""
-    parser = argparse.ArgumentParser(description='Update Pre-Alignment Queue')
-    parser.add_argument(
-        '-j',
-        dest='n_workers',
-        action='store',
-        required=False,
-        default=2,
-        type=int,
-        help='Number of workers [default 2]',
-    )
-    parser.add_argument(
-        '--host',
-        dest='host',
-        action='store',
-        required=False,
-        default='localhost',
-        help='Host running mongo database [Optional].'
-    )
-    parser.add_argument(
-        '--port',
-        dest='port',
-        action='store',
-        required=False,
-        type=int,
-        default=27022,
-        help='Mongo database port [Optional].'
-    )
-    return parser.parse_args()
 
 
 def get_update_db_ids():
@@ -127,36 +183,77 @@ def check_indicator_file(srx, srr, pattern, *args, **kwargs):
     return np.nan, np.nan
 
 
+def check_abi_solid(srx, srr, *args, **kwargs):
+    """Check Fastq is Abi Solid."""
+    logger.info('Checking queue for Abi Solid')
+    import gzip
+    layout = Path(patterns['layout'].format(srx=srx, srr=srr))
+    abi_solid = Path(patterns['abi_solid'].format(srx=srx, srr=srr))
+    fname = Path(patterns['fastq']['r1'].format(srx=srx, srr=srr))
+    r2 = Path(patterns['fastq']['r2'].format(srx=srx, srr=srr))
+
+    if abi_solid.exists():
+        return
+
+    try:
+        with layout.open() as fh:
+            flag = fh.read().strip()
+
+        if flag == 'keep_R2':
+            fname = r2
+        with gzip.open(fname, 'rt') as fh:
+            _, l = fh.readline(), fh.readline()
+            if l.startswith('T'):
+                abi_solid.touch()
+    except (KeyError, FileNotFoundError):
+        pass
+
+    logger.info('Abi Solid Complete')
+
+
 def initialize_store():
     """Initialize the data store with ids and prealn/queue."""
     logger.info('Initialize data store.')
+    logger.info('Querying database for ids.')
     store['ids'] = get_update_db_ids()
-    store.put('prealn/queue', store['ids'], data_columns=True,
-              format='table')
+    logger.info('Adding ids to store.')
+    store.put('prealn/queue', store['ids'], data_columns=True, format='table')
+    logger.info('Initialization complete')
 
-    # Add flag files
-    for key in 'layout', 'strand':
-        dat = dask_run(store['prealn/queue'], check_flag_file, patterns[key])
 
-        df = pd.DataFrame(dat, columns=['srx', 'srr', key])\
-            .set_index(['srx', 'srr'])\
-            .iloc[:, 0]\
-            .dropna()
+def append_store():
+    """Download new data and append to store."""
+    logger.info('Append new ids to data store.')
+    curr_ids = store['ids'].copy()
 
-        store.put(key, df, data_columns=True, format='t')
+    logger.info('Querying database for ids.')
+    ids = get_update_db_ids()
+    store['ids'] = ids
 
-    # Add indicator files
-    for key in ['download_bad', 'alignment_bad', 'quality_scores_bad',
-                'abi_solid']:
-        dat = dask_run(store['prealn/queue'], check_indicator_file,
-                       patterns[key])
+    # Sometimes an id is removed from the SRA, need to make sure to remove it
+    # from the queue.
+    srr_no_longer_in_store = curr_ids.srr.isin(ids.srr)
+    removed_ids = curr_ids[~srr_no_longer_in_store]
 
-        df = pd.DataFrame(dat, columns=['srx', 'srr'])\
-            .dropna()\
-            .reset_index(drop=True)
+    if len(removed_ids) > 0:
+        logger.info(
+            'There are {:,} ids no longer in the SRA.'.format(len(removed_ids))
+        )
+        srrs = removed_ids.srr
+        store.remove('prealn/queue', 'srr == srrs')
 
-        store_key = 'prealn/' + key
-        store.put(store_key, df, data_columns=True, format='t')
+    # Do not add ids already in the store back to the queue.
+    srr_already_in_store = ids.srr.isin(curr_ids.srr)
+    new_ids = ids[~srr_already_in_store]
+
+    if len(new_ids) == 0:
+        logger.info('There are no new ids.')
+        return
+
+    logger.info('There are {:,} new ids.'.format(len(new_ids)))
+    logger.info('Adding ids to store.')
+    store.append('prealn/queue', new_ids, data_columns=True, format='table')
+    logger.info('Initialization complete')
 
 
 def check_outputs(srx, srr, *args, **kwargs):
@@ -171,52 +268,200 @@ def check_outputs(srx, srr, *args, **kwargs):
 def process_outputs():
     """Check if all output files are present and update queues."""
     logger.info('Checking outputs.')
-    queue = store['prealn/queue'].copy()
-    res = dask_run(queue, check_outputs)
+    res = dask_run(store['prealn/queue'], check_outputs)
     ids = pd.DataFrame(res, columns=['srx', 'srr']).dropna()
 
-    srrs = ids.srr
-    store.remove('prealn/queue', 'srr == srrs')
-    store.append('prealn/complete', ids, data_columns=True, format='t')
-    store.append('aln/queue', ids, data_columns=True, format='t')
-    logger.info('{:,} SRRs removed from queue'.format(len(srrs)))
+    mask = store['prealn/queue'].srr.isin(ids.srr)
+    cnt = mask.sum()
+    if cnt > 0:
+        store.remove('prealn/queue', mask)
+        logger.info('{:,} SRRs removed from queue.'.format(cnt))
+        store.append('prealn/complete', ids, data_columns=True, format='t')
+        store.append('aln/queue', ids, data_columns=True, format='t')
     logger.info('Complete')
 
 
 def process_flags():
     """Check all flag and indicator files and store results."""
     # flag files
-    queue = store['prealn/queue'].copy()
     for key in 'layout', 'strand':
         logger.info(f'Checking {key} files.')
-        dat = dask_run(queue, check_flag_file, patterns[key])
+        dat = dask_run(store['prealn/queue'], check_flag_file, patterns[key])
         df = pd.DataFrame(dat, columns=['srx', 'srr', key])\
             .set_index(['srx', 'srr'])\
             .iloc[:, 0]\
             .dropna()
 
-        idx = store[key].index.get_level_values('srr')
-        srr_already_in_store = df.index.get_level_values('srr').isin(idx)
-        store.append(key, df[~srr_already_in_store], data_columns=True,
-                     format='t')
+        # Prevent duplicate entries in the store
+        if store.__contains__(key):
+            idx = store[key].index.get_level_values('srr')
+            srr_already_in_store = df.index.get_level_values('srr').isin(idx)
+            df = df[~srr_already_in_store]
+
+        store.append(key, df, data_columns=True, format='t')
 
     # indicator files
     for key in ['download_bad', 'alignment_bad', 'quality_scores_bad',
                 'abi_solid']:
         logger.info(f'Checking {key} files.')
-        dat = dask_run(queue, check_indicator_file, patterns[key])
+        dat = dask_run(store['prealn/queue'], check_indicator_file,
+                       patterns[key])
         df = pd.DataFrame(dat, columns=['srx', 'srr'])\
             .dropna()\
             .reset_index(drop=True)
 
-        srrs = df.srr
+        # Prevent duplicate entries in the store
+        if store.__contains__(key):
+            idx = store[key].srr
+            srr_already_in_store = df.srr.isin(idx)
+            df = df[~srr_already_in_store]
+
+        mask = store['prealn/queue'].srr.isin(df.srr)
+        cnt = mask.sum()
         # remove form queue because an indicator files indicates that
         # something is something wrong.
-        if len(srrs) > 0:
+        if cnt > 0:
             store_key = 'prealn/' + key
             store.append(store_key, df, data_columns=True, format='t')
-            store.remove('prealn/queue', 'srr == srrs')
-            break
+
+            logger.info('{:,} SRRs removed from queue.'.format(cnt))
+            store.remove('prealn/queue', mask)
+
+
+def update_queue():
+    logger.info('Updating queue.')
+    process_flags()
+    process_outputs()
+    logger.info('Update complete')
+
+
+def print_queue():
+    def parse_pairs(name, key):
+        try:
+            return name, store[key].shape[0]
+        except KeyError:
+            return name, 0
+
+    pairs = [
+        ("ids in the system", 'ids'),
+        ("queued", "prealn/queue"),
+        ("completed", 'prealn/complete'),
+        ("download bad", 'prealn/download_bad'),
+        ("quality scores bad", 'prealn/quality_scores_bad'),
+        ("alignment bad", 'prealn/alignment_bad'),
+        ("abi solid", 'prealn/abi_solid'),
+    ]
+    parsed = [parse_pairs(k, v) for k, v in pairs]
+
+    if store.__contains__('layout'):
+        layout = store['layout'].value_counts()
+        parsed.extend([
+            ("Single End", layout.SE),
+            ("Pair End", layout.PE),
+            ("Really Single End R1", layout.keep_R1),
+            ("Really Single End R2", layout.keep_R2),
+        ])
+
+    if store.__contains__('strand'):
+        strand = store['strand'].value_counts()
+
+        parsed.extend([
+            ("first strand", strand.same_strand),
+            ("second strand", strand.opposite_strand),
+            ("unstranded", strand.unstranded),
+        ])
+
+    report = '\nCurrent Queue Summary\n'
+    for k, v in parsed:
+        report += '{:,}\t\t\t{}\n'.format(v, k)
+
+    print(report)
+
+
+def agg_small(name, pattern, func):
+    logger.info(f'Checking {key} files.')
+    complete = store['prealn/complete']
+    dat = dask_run(complete, func, pattern)
+    df = pd.concat([x for x in dat if x is not None])
+
+    key = 'prealn/workflow/' + name
+    idx = store[key].index.get_level_values('srr')
+    srr_already_in_store = df.index.get_level_values('srr').isin(idx)
+#     store.append(key, df[~srr_already_in_store], data_columns=True, format='t')
+
+
+def store_small_data():
+    """Parse smaller datasets and add them to the store.
+
+    Smaller datasets are munged and added to their corresponding table in the
+    data store.
+    """
+    from ncbi_remap.parser import (
+        parse_fastq_summary,
+        parse_fastq_screen,
+        parse_hisat2,
+        parse_samtools_stats,
+        parse_bamtools_stats,
+        parse_picard_markduplicate_metrics,
+        parse_picardCollect_summary,
+        parse_featureCounts_summary
+    )
+
+    agg_small('fastq', patterns['fastq']['summary'], parse_fastq_summary)
+    agg_small('fastq_screen', patterns['fastq_screen'], parse_fastq_screen)
+    agg_small('hisat2', patterns['hisat2']['bam'] + '.log', parse_hisat2)
+    agg_small('samtools_stats', patterns['samtools_stats'],
+              parse_samtools_stats)
+    agg_small('bamtools_stats', patterns['bamtools_stats'],
+              parse_bamtools_stats)
+    agg_small('markduplicates',
+              patterns['picard']['markduplicates']['metrics'],
+              parse_picard_markduplicate_metrics)
+    agg_small('collectrnaseqmetrics/first',
+              patterns['picard']['collectrnaseqmetrics']['metrics']['first'],
+              parse_picardCollect_summary)
+    agg_small('collectrnaseqmetrics/second',
+              patterns['picard']['collectrnaseqmetrics']['metrics']['second'],
+              parse_picardCollect_summary)
+    agg_small(
+        'collectrnaseqmetrics/unstranded',
+        patterns['picard']['collectrnaseqmetrics']['metrics']['unstranded'],
+        parse_picardCollect_summary
+    )
+    agg_small('feature_counts/summary',
+              patterns['feature_counts']['summary'],
+              parse_featureCounts_summary)
+
+
+def agg_large(pattern, func):
+    pass
+
+
+def munge_big_data():
+    """Munge larger datasets for easier handling with dask.
+
+    Larger tables are not performant when added to the datastore. Dask does
+    well accessing multiple files on disk in parallel. Dask documentation
+    suggests to use the apache parquet. Here we munge the data and save it out
+    to the parquet format.
+    """
+    from ncbi_remap.parser import (
+        parse_samtools_idxstats,
+        parse_featureCounts_counts,
+        parse_featureCounts_jcounts,
+    )
+
+    agg_large(patterns['samtools_idxstats'], parse_samtools_idxstats)
+    agg_large(patterns['feature_counts']['counts'], parse_featureCounts_counts)
+    agg_large(patterns['feature_counts']['jcounts'],
+              parse_featureCounts_jcounts)
+
+
+def agg_data():
+    logger.info('Aggregating data tables.')
+    store_small_data()
+    munge_big_data()
+    logger.info('Aggregation complete.')
 
 
 if __name__ == '__main__':
@@ -225,10 +470,20 @@ if __name__ == '__main__':
     store = pd.HDFStore(DATA_STORE)
     patterns = get_patterns('patterns.yaml')
 
-    # Initialize if needed.
-    if not store.__contains__('ids'):
-        logger.info('Initialize data store.')
-        initialize_store()
+    if args.command == 'init':
+        if args.append:
+            append_store()
+        else:
+            initialize_store()
+    elif args.command == 'queue':
+        if args.queue_update:
+            update_queue()
+        elif args.queue_print:
+            print_queue()
+        elif args.queue_abi:
+            dask_run(store['prealn/queue'], check_abi_solid)
+    elif args.command == 'data':
+        agg_data()
 
-    process_flags()
-    process_outputs()
+    store.close()
+    daskClient.close()
