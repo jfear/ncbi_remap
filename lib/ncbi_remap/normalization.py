6621fd9b0680c343b334ceb06ec2df78e512b587
Justin Fear <justin.m.fear@gmail.com>
2018-03-14 17:43:00 -0400

    Fixes RPKM and adds tpm.

--- lib/ncbi_remap/normalization.py
+++ lib/ncbi_remap/normalization.py
@@ -64,4 +64,41 @@ def rpkm(df, gene_length, scale_library=1e6, scale_length=1e3, log=None):
     elif log == 'ln':
         log = np.log
 
-    return log(df / (totals / scale_library) / (gene_length / scale_length) + 1)
+    return log((df / (totals / scale_library)).T / (gene_length / scale_length) + 1).T
+
+
+def tpm(df, gene_length, scale_library=1e6, scale_length=1e3, log=None):
+    """Transcripts Per Killobase Million.
+
+    Calcualtes TPM which normalizes by library size and gene length.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        A DataFrame with genes as rows and samples as columns.
+    gene_length : pd.Series
+        Series were index matches df.index and values are gene lengths.
+    scale_library : int or float
+        Scaling factor to scale the library size.
+    scale_length : int or float
+        Scaling factor to scale gene model lengths.
+    log : None or function or str
+        If a function is giving will apply this to the data before returning.
+        If a string is given then it uses numpy log functions that correspond
+        to the provided string.
+
+    """
+
+    rpk = (df.T / (gene_length / scale_length)).T
+    totals = rpk.sum()
+
+    if log is None:
+        log = lambda x: x - 1
+    elif log == 'log2':
+        log = np.log2
+    elif log == 'log10':
+        log = np.log10
+    elif log == 'ln':
+        log = np.log
+
+    return log((rpk / (totals / scale_library)) + 1)
