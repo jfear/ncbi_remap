39667784a19bfdb3b3da387eb6f8700e1d13f78b
Justin Fear <justin.m.fear@gmail.com>
2018-06-26 08:27:34 -0400

    Moves the cache to homedir for faster processing.

--- lib/ncbi_remap/config.py
+++ lib/ncbi_remap/config.py
@@ -7,7 +7,7 @@ from joblib import Memory
 # Useful directories
 PROJECT_DIR = Path(__file__).absolute().parents[2].as_posix()
 CONFIG_DIR = Path(PROJECT_DIR, 'config').as_posix()
-CACHE_DIR = Path(PROJECT_DIR, 'output/cache').as_posix()
+CACHE_DIR = Path('~/.cache').expanduser().as_posix()
 
 # Make missing dirs
 Path(CACHE_DIR).mkdir(exist_ok=True, parents=True)
