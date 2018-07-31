9be9e1915f55b58820379f278872d5b24a8dec4f
Justin Fear <justin.m.fear@gmail.com>
2018-07-31 13:00:26 -0400

    Makes library pip installable.

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='ncbi_remap',
+    version='0.0.1',
+    description="Local library for the NCBI Remap project",
+    author="Justin M Fear",
+    author_email='justin.m.fear@gmail.com',
+    url='https://github.com/jfear/ncbi_remap',
+    packages=['ncbi_remap'],
+    license="MIT license",
+)
