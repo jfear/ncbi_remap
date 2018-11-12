d4c4c12ee101d38a0f87f7d1bd1003c87f5aa691
Justin Fear <justin.m.fear@gmail.com>
2018-11-12 11:16:01 -0500

    Starts RNA-Seq downstream analysis by running stringtie.

---
+++ rnaseq-analysis-wf/scripts/stringtie.py
@@ -0,0 +1,25 @@
+"""Script to generate stringtie command."""
+from pathlib import Path
+
+from ncbi_remap.snakemake import get_flag
+
+pth = Path(f'../output/prealn-wf/samples/{snakemake.wildcards.srx}')
+glob = list(pth.glob('*/STRAND'))
+strand = get_flat(glob[0])
+
+if strand == 'samestrand':
+    param = '--rf'
+elif strand == 'oppositestrand':
+    param = '--fr'
+else:
+    param = ''
+
+
+cmd = f"""stringtie \
+    {snakemake.input.bam} \
+    -G {snakemake.input.gtf} \
+    {param} \
+    -p {snakemake.threads} \
+    -o {snakemake.output[0]}
+"""
+shell(cmd)
