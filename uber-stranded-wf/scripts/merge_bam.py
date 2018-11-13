a94a71a4891b7542302483e102819e68f148ee0e
Justin Fear <justin.m.fear@gmail.com>
2018-11-13 12:08:57 -0500

    Adds Bam merging.

---
+++ uber-stranded-wf/scripts/merge_bam.py
@@ -0,0 +1,26 @@
+"""Script to run samtools merge on a large number of BAMs."""
+import os
+from tempfile import NamedTemporaryFile
+
+from snakemake.shell import shell
+
+TMPDIR = os.path.join('/lscratch', os.getenv('SLURM_JOBID'))
+shell.prefix("set -euo pipefail; export TMPDIR={};".format(TMPDIR))
+
+threads = snakemake.get('threads', 1)
+
+bams = list(snakemake.input)
+
+tmp = NamedTemporaryFile(mode='w', dir=TMPDIR)
+tmp.write('\n'.join(bams))
+
+cmd = f"""samtools merge \
+    -b {tmp.name} \
+    -@ {threads} \
+    {snakemake.output[0]}
+"""
+
+print(cmd)
+shell(cmd)
+
+tmp.close()
---
+++ uber-stranded-wf/scripts/stringtie.py
@@ -0,0 +1,33 @@
+"""Script to generate stringtie command."""
+from pathlib import Path
+
+from snakemake.shell import shell
+
+from ncbi_remap.snakemake import get_flag
+
+pth = Path(f'../output/prealn-wf/samples/{snakemake.wildcards.srx}')
+glob = list(pth.glob('*/STRAND'))
+
+try:
+    strand = get_flag(glob[0])
+except IndexError:
+    strand = ''
+
+if strand == 'samestrand':
+    param = '--rf'
+elif strand == 'oppositestrand':
+    param = '--fr'
+else:
+    param = ''
+
+
+cmd = f"""stringtie \
+    {snakemake.input.bam} \
+    -G {snakemake.input.gtf} \
+    {param} \
+    -p {snakemake.threads} \
+    -o {snakemake.output[0]}
+"""
+
+print(cmd)
+shell(cmd)
