24d36c447a97a29325b5a1f981dd8a31c4419cfa
Justin Fear <justin.m.fear@gmail.com>
2019-02-04 17:08:32 -0500

    Adds notebook to pull test samples for optimizing stringtie and salmon.

---
+++ notebook/2019-01-30_create_sampletable_for_fly_tie.py
@@ -0,0 +1,70 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.2'
+#       jupytext_version: 0.8.6
+#   kernelspec:
+#     display_name: Python [conda env:ncbi_remap]
+#     language: python
+#     name: conda-env-ncbi_remap-py
+# ---
+
+# %% [markdown]
+# # Sample Table for Fly Tie.
+
+# %% [markdown]
+# I setup a new repository [fly_tie](https://github.com/jfear/fly_tie) to run StringTie optimizations. I want to use head data from the SRA for this process. Here I need to generate a set of ~100 high quality samples of head data for this process.
+
+# %%
+import os
+import sys
+from pathlib import Path
+
+from IPython.display import display, HTML, Markdown
+import numpy as np
+import pandas as pd
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Project level imports
+from ncbi_remap.notebook import Nb
+from ncbi_remap.plotting import make_figs
+
+# %%
+# Setup notebook
+nbconfig = Nb.setup_notebook()
+
+# Connect to data store
+store = pd.HDFStore('../output/sra.h5', mode='r')
+
+# %%
+# Pull out head data
+metadata = pd.read_csv('../output/geo-wf/rnaseq_metadata.tsv', sep='\t', index_col=0)
+srxs = metadata.query('tissue == "head"').index.tolist()
+
+# %%
+# Grab only the stranded head data.
+pct_strand = store.select('/prealn/workflow/collectrnaseqmetrics/second', where='srx == srxs')['PCT_CORRECT_STRAND_READS'].to_frame().reset_index()
+stranded = pct_strand.loc[pct_strand.PCT_CORRECT_STRAND_READS > .9, ['srx', 'srr']]
+
+# %%
+# Add on library layout info for mapping
+layout = store.select('layout', where='srx == srxs & layout == ["PE", "SE"]').to_frame().reset_index()
+stranded_w_layout = stranded.merge(layout, on=['srx', 'srr'], how='left').dropna()
+stranded_w_layout.columns = ['samplename', 'Run', 'layout']
+
+#%%
+# Remove white space
+stranded_w_layout.samplename = stranded_w_layout.samplename.str.strip()
+stranded_w_layout.Run = stranded_w_layout.Run.str.strip()
+
+# %%
+stranded_w_layout.to_csv('../output/notebook/2019-01-30_269_head_samples_for_fly_tie.tsv', sep='\t', index=False)
+
+# %%
+
