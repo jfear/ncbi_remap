9a3638f36690d4273b270f9a1a8df517764f16ce
Justin M. Fear <justin.m.fear@gmail.com>
2019-11-29 17:23:32 -0500

    Download corpus.
    I was getting errors related to nltk needing to download
    the corpus. I add logic to handle this.

--- library_strategy-wf/scripts/free_text_library_strategy.py
+++ library_strategy-wf/scripts/free_text_library_strategy.py
@@ -15,45 +15,25 @@ import pandas as pd
 
 from pymongo import MongoClient
 
+import nltk
 from nltk.tokenize import regexp_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 
 from ncbi_remap.nlp import lookups
 
-
-def get_documents(doc):
-    srx = doc['_id']
-    del doc['_id']
-
-    for k, v in doc.items():
-        if v is None:
-            doc[k] = ''
-
-    txt = ' '.join(doc.values()).lower()\
-        .replace('_', ' ')\
-        .replace('hi-c', 'hic')\
-        .replace('3-c', '3c')\
-        .replace('4-c', '4c')\
-        .replace("3'", '3prime')\
-        .replace("5'", '5prime')\
-        .replace('-', ' ')\
-        .replace('sequencing', 'seq')\
-        .replace('sequenced', 'seq')\
-        .replace('sequence', 'seq')
-
-    # Translate based on known phrases in lookup table
-    for k, v in lookups['library_strategy'].items():
-        txt = txt.replace(k, v)
-
-    return srx, txt
-
+try:
+    stopwords.words("english")
+except:
+    nltk.download("stopwords")
 
 def main(ncbi):
     # Build a list of documents
     documents = [get_documents(x) for x in ncbi.aggregate([
         {
             '$project': {
+                '_id': False,
+                'srx': '$srx',
                 'exp_title': '$title',
                 'exp_design': '$design',
                 'exp_library_name': '$library_name',
@@ -63,6 +43,7 @@ def main(ncbi):
     ])]
 
     # tokenize documents
+    download_corpus()
     eng_stops = stopwords.words('english') + lookups['stopwords']
     wordnets_lemmatizer = WordNetLemmatizer()
 
@@ -96,7 +77,7 @@ def main(ncbi):
         # count the number of times a strategy appears and keep only the top ones.
         arr, cnt = np.unique(np.array(token_strategies), return_counts=True)
         cnts = list(zip(arr, cnt))
-        # Pull out the best(s) library strategy based on number occurances
+        # Pull out the best(s) library strategy based on number occurrences
         if len(cnts) > 0:
             keeps = []
             high = 0
@@ -116,6 +97,44 @@ def main(ncbi):
     df.index.name = 'SRX'
     df.columns = ['freetxt_library_strategy']
     df.to_parquet(snakemake.output[0])
+
+
+def download_corpus():
+    try:
+        nltk.data.find("corpora/stopwords")
+    except LookupError:
+        nltk.download("stopwords")
+
+    try:
+        nltk.data.find("corpora/wordnet")
+    except LookupError:
+        nltk.download("wordnet")
+
+def get_documents(doc):
+    srx = doc['srx']
+    del doc['srx']
+
+    for k, v in doc.items():
+        if v is None:
+            doc[k] = ''
+
+    txt = ' '.join(doc.values()).lower()\
+        .replace('_', ' ')\
+        .replace('hi-c', 'hic')\
+        .replace('3-c', '3c')\
+        .replace('4-c', '4c')\
+        .replace("3'", '3prime')\
+        .replace("5'", '5prime')\
+        .replace('-', ' ')\
+        .replace('sequencing', 'seq')\
+        .replace('sequenced', 'seq')\
+        .replace('sequence', 'seq')
+
+    # Translate based on known phrases in lookup table
+    for k, v in lookups['library_strategy'].items():
+        txt = txt.replace(k, v)
+
+    return srx, txt
 
 
 if __name__ == '__main__':
