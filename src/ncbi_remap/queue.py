6b3574b15ff7ff44c6853d3bc4bca37da1885199
Justin M. Fear <justin.m.fear@gmail.com>
2019-12-03 18:32:10 -0500

    Cleans up and adds typing

--- src/ncbi_remap/queue.py
+++ src/ncbi_remap/queue.py
@@ -1,49 +1,52 @@
 from pathlib import Path
+from typing import Callable, List, Tuple
 
 import numpy as np
-from dask import delayed
+from pandas import DataFrame
+from dask.distributed import Client
 
 
-def dask_run_srr_checker(ids, checker, pattern, client):
+def dask_run_srr_checker(
+    ids: DataFrame, checker: Callable, pattern: str, client: Client
+) -> List[DataFrame]:
     """Helper function to run function using dask."""
-    lazy = []
-    for idx, (srx, srr) in ids.iterrows():
-        lazy.append(delayed(checker)(srx, srr, pattern))
-    return client.gather(client.compute(lazy))
+    futures = client.map(lambda x: checker(x["srx"], x["srr"], pattern), ids.to_dict("records"))
+    return client.gather(futures)
 
 
-def dask_run_srx_checker(srxs, checker, client):
+def dask_run_srx_checker(srxs: List, checker: Callable, client: Client) -> List[DataFrame]:
     """Helper function to run function using dask."""
-    lazy = []
-    for srx in srxs:
-        lazy.append(delayed(checker)(srx))
-    return client.gather(client.compute(lazy))
+    futures = client.map(checker, srxs)
+    return client.gather(futures)
 
 
-def build_parsed(fname, parser, **kwargs):
+def build_parsed(fname: str, parser: Callable, **kwargs) -> DataFrame:
     df = parser(fname)
     if df is None:
         return None
     return df.assign(**kwargs).set_index(sorted(kwargs.keys(), reverse=True))
 
 
-def dask_run_srr_parser(ids, parser, pattern, client):
+def dask_run_srr_parser(
+    ids: DataFrame, parser: Callable, pattern: str, client: Client
+) -> List[DataFrame]:
     """Helper function to run function using dask."""
     futures = client.map(lambda x: build_parsed(parser, pattern, **x), ids.to_dict("records"))
     return client.gather(futures)
 
 
-def dask_run_srx_parser(srxs, parser, pattern, client):
+def dask_run_srx_parser(
+    srxs: List[str], parser: Callable, pattern: str, client: Client
+) -> List[DataFrame]:
     """Helper function to run function using dask."""
     futures = client.map(lambda srx: build_parsed(parser, pattern, srx=srx), srxs)
     return client.gather(futures)
 
 
-def check_indicator_file(srx, srr, pattern):
+def check_indicator_file(srx: str, srr: str, pattern: str) -> Tuple:
     """Check if an indicator file is present."""
     fname = Path(pattern.format(srx=srx, srr=srr))
     if fname.exists():
         return srx, srr
 
     return np.nan, np.nan
-
