3322fb5de8b7e3e740bf6d811bf99c94ae9b2952
Justin M. Fear <justin.m.fear@gmail.com>
2020-01-20 15:25:47 -0500

    Updated to use prealn-wf information only.
    Updates the feature set to include the features only
    from the prealn-wf.

--- library_strategy-wf/scripts/build_library_strategy_feature_set.py
+++ library_strategy-wf/scripts/build_library_strategy_feature_set.py
@@ -28,6 +28,8 @@ Features include:
     * All Intergenic regions
 """
 import os
+from pathlib import Path
+
 import numpy as np
 import pandas as pd
 
@@ -37,7 +39,7 @@ def main():
     store = pd.HDFStore(snakemake.input[0], mode="r")
 
     # Generate a list of completed SRXs
-    srxs = store["aln/complete"].srx.unique().tolist()
+    srxs = store["prealn/complete"].srx.unique().tolist()
 
     # CollectRNASeqMetrics
     cols = [
@@ -50,16 +52,28 @@ def main():
         "MEDIAN_5PRIME_BIAS",
         "MEDIAN_3PRIME_BIAS",
     ]
-    cm = store.select(
-        "prealn/workflow/collectrnaseqmetrics/unstranded", where="srx == srxs", columns=cols
+    cm = (
+        store.select(
+            "prealn/workflow/collectrnaseqmetrics/unstranded", where="srx == srxs", columns=cols
+        )
+        .groupby("srx")
+        .median()
     )
 
     # CollectRNASeqMetrics Gene Body Coverage
-    gb = store.select("prealn/workflow/collectrnaseqmetrics/genebody", where="srx == srxs")
+    gb = (
+        store.select("prealn/workflow/collectrnaseqmetrics/genebody", where="srx == srxs")
+        .groupby("srx")
+        .median()
+    )
 
     # Markduplicates
     cols = ["PERCENT_DUPLICATION"]
-    mark = store.select("prealn/workflow/markduplicates", where="srx == srxs", columns=cols)
+    mark = (
+        store.select("prealn/workflow/markduplicates", where="srx == srxs", columns=cols)
+        .groupby("srx")
+        .median()
+    )
 
     # FeatureCounts Summary
     cols = [
@@ -69,46 +83,69 @@ def main():
         "Unassigned_NoFeatures",
         "Unassigned_Unmapped",
     ]
-    feature_summary = store.select(
-        "prealn/workflow/feature_counts/summary", columns=cols, where="srx == srxs"
+    feature_summary = (
+        store.select("prealn/workflow/feature_counts/summary", columns=cols, where="srx == srxs")
+        .groupby("srx")
+        .median()
     )
 
-    # Munge Together by SRR
-    dat_by_srr = cm.join(gb).join(mark).join(feature_summary)
-    dat_by_srr_no_na = dat_by_srr.fillna(dat_by_srr.mean().to_dict(), axis=0)
-    dat_by_srr_no_na.shape
+    pd.concat([cm, gb, mark, feature_summary], axis=1).isnull()
 
     # FeatureCount Coverage Counts
-    genic = pd.read_parquet("../output/aln-downstream-wf/aggregate_genic_counts.parquet").reindex(
-        srxs
-    )["count"]
-    genic.name = "genic"
-    genic = genic.astype(np.float64)
+    genic_counts = aggregate_feature_counts(snakemake.params.genic_counts, srxs).rename(
+        {"count": "num_genic_reads", "prop_on": "prop_genes_on"}, axis=1
+    )
 
-    intergenic = pd.read_parquet(
-        "../output/aln-downstream-wf/aggregate_intergenic_counts.parquet"
-    ).reindex(srxs)["count"]
-    intergenic.name = "intergenic"
-    intergenic = intergenic.astype(np.float64)
-
-    junctions = pd.read_parquet(
-        "../output/aln-downstream-wf/aggregate_junction_counts.parquet"
-    ).reindex(srxs)["count"]
-    junctions.name = "junctions"
-    junctions = junctions.astype(np.float64)
-
-    coverage = pd.concat([genic, intergenic, junctions], axis=1)
+    junction_counts = (
+        aggregate_feature_counts(snakemake.params.junction_counts, srxs)
+        .drop("prop_on", axis=1)
+        .rename({"count": "num_junction_reads"}, axis=1)
+    )
 
     # Make final feature set aggregated to SRX
-    features = dat_by_srr_no_na.reset_index().groupby("srx").mean().join(coverage)
+    features = pd.concat(
+        [cm, gb, mark, feature_summary, genic_counts, junction_counts], axis=1, sort=False
+    ).rename_axis("srx")
 
     features.to_parquet(snakemake.output[0])
+
+
+def aggregate_feature_counts(count_dir: str, srxs: list) -> pd.DataFrame:
+    return pd.concat(
+        (
+            read_feature_counts(pth)
+            for pth in Path(count_dir).glob("*.parquet")
+            if check_srx_in_list(pth, srxs)
+        ),
+        axis=0,
+        sort=False,
+    )
+
+
+def read_feature_counts(file_name: Path) -> pd.DataFrame:
+    return (
+        pd.read_parquet(file_name)
+        .groupby("srx")
+        .agg({"count": "sum"})
+        .assign(prop_on=lambda x: (x["count"] > 0).mean())
+    )
+
+
+def check_srx_in_list(file_name: Path, srxs: list) -> bool:
+    if file_name.stem in srxs:
+        return True
 
 
 if __name__ == "__main__":
     if os.getenv("SNAKE_DEBUG", False):
         from ncbi_remap.debug import snakemake_debug
 
-        snakemake = snakemake_debug(workdir="library_strategy-wf", input="../output/sra.h5")
+        snakemake = snakemake_debug(
+            input="../../output/sra.h5",
+            params=dict(
+                genic_counts="../../output/prealn-wf/gene_counts",
+                junction_counts="../../output/prealn-wf/junction_counts",
+            ),
+        )
 
     main()
