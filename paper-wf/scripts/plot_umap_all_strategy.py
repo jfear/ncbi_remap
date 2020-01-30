c88a2a3d121924cc52ff74e982287c96a6780a78
Justin M. Fear <justin.m.fear@gmail.com>
2020-01-30 15:44:01 -0500

    Makes UMAP plot of library strategy.

--- paper-wf/scripts/plot_umap_all_strategy.py
+++ paper-wf/scripts/plot_umap_all_strategy.py
@@ -3,21 +3,18 @@ import os
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-from ncbi_remap.plotting import umap
+from ncbi_remap.plotting import sra_umap
 
 
 def main():
     plt.style.use(snakemake.params.get("style", "sra"))
 
-    plot = umap.Plot(
-        features_file=snakemake.input.features,
-        labels_file=snakemake.input.sra_labels,
+    plot = sra_umap.Plot(
+        embeddings_file=snakemake.input.embeddings,
+        labels_file=snakemake.input.labels,
         plot_kwargs=dict(snakemake.params.get("plot_kwargs", {})),
         ax_kwargs=dict(snakemake.params.get("ax_kwargs", {})),
     )
-
-    if "title" in snakemake.params:
-        plt.suptitle(snakemake.params.title
 
     plt.savefig(snakemake.output[0])
 
@@ -30,9 +27,9 @@ if __name__ == "__main__":
 
         snakemake = snakemake_debug(
             input=dict(
-                features="../../output/library_strategy-wf/prealn_feature_set.parquet",
-                sra_labels="../../output/library_strategy-wf/sra_strategy_selection.parquet,
-            )
+                embeddings="../output/library_strategy-wf/umap_prealn_features_embeddings.tsv",
+                labels="../output/library_strategy-wf/sra_strategy_selection.parquet"
+            ),
             params=dict(
                 plot_kwargs=dict(),
                 ax_kwargs=dict(),
