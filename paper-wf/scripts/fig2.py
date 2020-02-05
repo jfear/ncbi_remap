e300eb1a861b937008cf7c68c3fc4877bda2cba4
Justin M. Fear <justin.m.fear@gmail.com>
2020-02-05 09:04:58 -0500

    Mocks out fig2

--- paper-wf/scripts/fig2.py
+++ paper-wf/scripts/fig2.py
@@ -9,35 +9,40 @@ def main():
     panel_labels_kwargs = dict(
         size=snakemake.params.label_size,
         weight=snakemake.params.label_weight,
-        font=snakemake.params.label_font
+        font=snakemake.params.label_font,
     )
 
     figsize = FigSize(snakemake.params.figsize)
 
-    svg.Figure(figsize.width_cm, figsize.height_cm, 
+    svg.Figure(
+        figsize.width_cm,
+        figsize.height_cm,
         svg.Panel(
-            svg.SVG(snakemake.input.drawing).scale(3.5).move(10, 10),
+            svg.SVG(snakemake.input.umap_library_strategy_panel).move(10, 10),
             svg.Text("A", 5, 10, **panel_labels_kwargs),
-            svg.Text("B", 154, 27, **panel_labels_kwargs),
-            svg.Text("C", 154, 97, **panel_labels_kwargs),
-            svg.Text("D", 154, 167, **panel_labels_kwargs),
         ),
         svg.Panel(
-            svg.SVG(snakemake.input.distribution).scale(1.3).move(10, 10),
-            svg.Text("E", 5, 10, **panel_labels_kwargs),
-        ).move(410, 0),
+            svg.SVG(snakemake.input.isolation_score_panel).move(10, 10),
+            svg.Text("B", 5, 10, **panel_labels_kwargs),
+            svg.Text("C", 260, 10, **panel_labels_kwargs),
+        ).move(250, 0),
+        svg.Panel(),
+        svg.Panel(
+            svg.SVG(snakemake.input.importance).move(10, 10),
+            svg.Text("E", 35, 10, **panel_labels_kwargs),
+        ).move(480, 190),
     ).save(snakemake.output[0])
-
-
 
 
 if __name__ == "__main__":
     if os.getenv("SNAKE_DEBUG"):
         from ncbi_remap.debug import snakemake_debug
 
-        snakemake = snakemake_debug(input=dict(
-            drawing="../../data/drawings/overview_schematic.svg",
-            distribution="../../output/paper-wf/figure_panels/sample_submission.svg",
-        ))
+        snakemake = snakemake_debug(
+            input=dict(
+                drawing="../../data/drawings/overview_schematic.svg",
+                distribution="../../output/paper-wf/figure_panels/sample_submission.svg",
+            )
+        )
 
     main()
