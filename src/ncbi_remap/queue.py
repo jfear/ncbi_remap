0c9c502d0c1158285a9cef39c8a72664407a22b9
Justin M. Fear <justin.m.fear@gmail.com>
2020-02-07 10:56:56 -0500

    Adds helper function to create a file based queue.

--- src/ncbi_remap/queue.py
+++ src/ncbi_remap/queue.py
@@ -1,9 +1,46 @@
 from pathlib import Path
-from typing import Callable, List, Tuple
+from typing import Callable, List, Tuple, Optional
+import pickle
 
 import numpy as np
 from pandas import DataFrame
 from dask.distributed import Client
+
+
+def get_samples(
+    sample_file: str, done_folder: str, subset_file: Optional[str] = None, size: int = 100
+) -> List[str]:
+    """Create of list of samples that need to be run.
+
+    Parameters
+    ----------
+    sample_file : str
+        A pickle file with a set of samples.
+    done_folder : str
+        A folder where all filenames are indicators if a sample has completed
+        a workflow.
+    subset_file : str, optional
+        An optional pickle file with a subset of samples to focus on.
+    size : int, optional
+        The number of samples to return, by default 100
+
+    Returns
+    -------
+    A list of samples.
+
+    """
+    queued = pickle.load(open(sample_file, "rb"))  # type: set
+
+    pth = Path(done_folder)
+    if pth.exists():
+        completed = {x.stem for x in pth.iterdir()} # type: set
+        queued = queued - completed
+
+    if subset_file:
+        focus = pickle.load(open(subset_file, "rb"))  # type: set
+        queued = queued.intersection(focus)
+
+    return sorted(list(queued))[:size]
 
 
 def dask_run_srr_checker(
