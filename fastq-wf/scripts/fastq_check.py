d24978c025c49d4d5d0c1ed559913070d4668dbd
Justin M. Fear <justin.m.fear@gmail.com>
2020-02-14 10:09:52 -0500

    Updates fastq checking.

--- fastq-wf/scripts/fastq_check.py
+++ fastq-wf/scripts/fastq_check.py
@@ -1,18 +1,15 @@
 """Looks at R1 and R2 to determine library layout"""
 import os
-import sys
+import gzip
 from pathlib import Path
 from collections import namedtuple
 
-from ncbi_remap.fastq import check_fastq, fastq_stats, fastq_abi_solid
+from ncbi_remap.fastq import fastq_is_empty, fastq_read_stats, fastq_is_abi_solid
 from ncbi_remap.snakemake import put_flag
 
-DIRNAME = Path(snakemake.input.r1).parent
-OUTPUT_HEADER = "\t".join(
-    ["md5_R1", "libsize_R1", "avgLen_R1", "md5_R2", "libsize_R2", "avgLen_R2"]
-)
+SAMPLE_NAME = snakemake.wildcards.get("srr", snakemake.wildcards.get("srx"))
 
-ReadSummary = namedtuple("ReadSummary", "md5 size len abi")
+ReadSummary = namedtuple("ReadSummary", "library_size read_length")
 
 
 class DownloadException(Exception):
@@ -25,76 +22,94 @@ class AbiException(Exception):
 
 def main():
     try:
-        verify_download(snakemake.input.download_log)
+        verify_sra_download(snakemake.input.download_log)
         r1 = summarize(snakemake.input.r1)
         r2 = summarize(snakemake.input.r2)
         create_flags(r1, r2)
         save_summary(r1, r2)
-    except (DownloadException, AbiException):
-        Path(snakemake.input.sra).unlink()
-        Path(snakemake.input.r1).unlink()
-        Path(snakemake.input.r2).unlink()
+    except AbiException:
+        Path(snakemake.params.abi_solid, SAMPLE_NAME).touch()
+        remove_sample_due_to_problems()
+    except DownloadException:
+        Path(snakemake.params.download_bad, SAMPLE_NAME).touch()
+        remove_sample_due_to_problems()
 
 
-def verify_download(file_name):
-    with open(file_name) as fh:
+def verify_sra_download(download_log: str):
+    with open(download_log) as fh:
         text = fh.read()
-        if "download successfully" not in text:
-            Path(DIRNAME / "DOWNLOAD_BAD").touch()
+        if "downloaded successfully" not in text:
             raise DownloadException
 
 
-def summarize(file_name) -> ReadSummary:
-    # TODO: handle GZ files.
-    if check_fastq(file_name):
-        return ReadSummary(None, None, None)
+def summarize(fastq_gz: str) -> ReadSummary:
+    """Summarize Gzip'd FASTQ files."""
+    with gzip.open(fastq_gz, "rt") as fastq_handle:
+        if fastq_is_empty(fastq_handle):
+            return ReadSummary(0, 0)
 
-    lib_size, avg_len = fastq_stats(file_name)
-    abi = fastq_abi_solid(file_name)
-    return ReadSummary("Deprecated", lib_size, avg_len, abi)
+        fastq_handle.seek(0, 0)  # Go back to the begining of the file
+        if fastq_is_abi_solid(fastq_handle):
+            raise AbiException
+
+        fastq_handle.seek(0, 0)  # Go back to the begining of the file
+        library_size, avg_read_length = fastq_read_stats(fastq_handle)
+        return ReadSummary(library_size, avg_read_length)
 
 
 def create_flags(r1: ReadSummary, r2: ReadSummary):
-    if (r1.size > 1000) & (r2.size is None) & (r1.len > 10) & (r2.len is None):
-        # R2 does not exists so SE
-        put_flag(snakemake.output.layout, "SE")
-
-    elif (r2.size is None) & (r2.len is None):
-        # SE but R1 looks bad, flag as download bad
-        Path(DIRNAME, "DOWNLOAD_BAD").touch()
-        raise DownloadException
-
-    elif (r1.size > 1000) & (r2.size > 1000) & (r1.len > 10) & (r2.len > 10):
-        if r1.size == r2.size:
-            # Both reads look good so PE
-            put_flag(snakemake.output.layout, "PE")
+    if (r1.library_size > 1000) & (r1.read_length > 10) & (r2.library_size == 0):
+        # SE because R1 looks good and R2 is empty
+        layout = "SE"
+    elif (
+        (r1.library_size > 1000)
+        & (r1.read_length > 10)
+        & (r2.library_size > 1000)
+        & (r2.read_length > 10)
+    ):
+        if r1.library_size == r2.library_size:
+            # PE because R1 and R2 look good and have the same number of reads
+            layout = "PE"
         else:
-            # There are an uneven number of reads between R1 and R2.
-            # Instead of messing with this, just consider SE and use R1.
-            put_flag(snakemake.output.layout, "keep_R1")
-
-    elif (r1.size > 1000) & (r1.len > 10):
-        # Only R1 looks ok, so SE
-        put_flag(snakemake.output.layout, "keep_R1")
-
-    elif (r2.size > 1000) & (r2.len > 10):
-        # Only R2 looks ok, consider single-end
-        put_flag(snakemake.output.layout, "keep_R2")
-
+            # SE/keep_R1 because R1 and R2 look good, but uneven number of reads
+            layout = "keep_R1"
+    elif (r1.library_size > 1000) & (r1.read_length > 10):
+        # SE/keep_R1 because only R1 looks good
+        layout = "keep_R1"
+    elif (r2.library_size > 1000) & (r2.read_length > 10):
+        # SE/keep_R2 because only R2 looks good
+        layout = "keep_R2"
     else:
-        # R1 and R2 look bad, flag as download bad
-        Path(DIRNAME, "DOWNLOAD_BAD").touch()
+        # We have a problem because R1 and R2 look bad
         raise DownloadException
 
-    if r1.abi | r2.abi:
-        Path(DIRNAME, "ABI_SOLID").touch()
-        raise AbiException
+    put_flag(snakemake.output.layout, layout)
 
 
 def save_summary(r1: ReadSummary, r2: ReadSummary):
     with open(snakemake.output.summary, "w") as file_out:
-        file_out.write(OUTPUT_HEADER + "\n")
-        file_out.write("\t".join([*r1[:-1], *r2[:-1]] + "\n"))
+        file_out.write("\t".join(["libsize_R1", "avgLen_R1", "libsize_R2", "avgLen_R2"]))
+        file_out.write("\n")
+        file_out.write(
+            "\t".join(
+                [
+                    str(r1.library_size),
+                    str(r1.read_length),
+                    str(r2.library_size),
+                    str(r2.read_length),
+                ]
+            )
+        )
+        file_out.write("\n")
+
+
+def remove_sample_due_to_problems():
+    try:
+        Path(snakemake.input.sra).unlink()
+        Path(snakemake.input.r1).unlink()
+        Path(snakemake.input.r2).unlink()
+    except FileNotFoundError:
+        pass
 
 
 if __name__ == "__main__":
@@ -108,10 +123,16 @@ if __name__ == "__main__":
                 r2="../../output/fastq-wf/fastqs/SRR031744_2.fastq.gz",
                 download_log="../../output/fastq-wf/sra_download_logs/SRR031744.log",
             ),
+            params=dict(
+                download_bad="../../output/fastq-wf/download_bad",
+                abi_solid="../../output/fastq-wf/abi_solid",
+            ),
             output=dict(
                 layout="../../output/fastq-wf/fastq_info/SRR031744/LAYOUT",
                 summary="../../output/fastq-wf/fastq_info/SRR031744/summary.tsv",
             ),
         )
 
+    Path(snakemake.params.download_bad).mkdir(exist_ok=True)
+    Path(snakemake.params.abi_solid).mkdir(exist_ok=True)
     main()
