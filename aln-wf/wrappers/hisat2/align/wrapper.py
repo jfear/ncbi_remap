01e1d5b214546525394cedcad3ab61dbd087787b
Justin M. Fear <justin.m.fear@gmail.com>
2020-02-14 16:43:58 -0500

    Cleans up wrapper code.

--- aln-wf/wrappers/hisat2/align/wrapper.py
+++ aln-wf/wrappers/hisat2/align/wrapper.py
@@ -3,55 +3,51 @@ __copyright__ = "Copyright 2016, Justin Fear"
 __email__ = "justin.fear@nih.gov"
 __license__ = "MIT"
 
-import sys
 from tempfile import NamedTemporaryFile
 from snakemake.shell import shell
 from lcdblib.snakemake import aligners
 
-sys.path.insert(0, '../src')
 from ncbi_remap.snakemake import get_flag
 
-inputs = snakemake.input
-outputs = snakemake.output
-hisat2_extra = snakemake.params.get('hisat2_extra', '')
-samtools_view_extra = snakemake.params.get('samtools_view_extra', '')
-samtools_sort_extra = snakemake.params.get('samtools_sort_extra', '')
+r1 = snakemake.input.r1
+r2 = snakemake.input.r2
+hisat2_extra = snakemake.params.get("hisat2_extra", "")
+samtools_view_extra = snakemake.params.get("samtools_view_extra", "")
+samtools_sort_extra = snakemake.params.get("samtools_sort_extra", "")
 
 log = snakemake.log_fmt_shell()
 
 # Look up Layout
-flag = get_flag(inputs.layout)
+layout = get_flag(snakemake.input.layout)
 
-if flag == 'PE':
-    in_R1 = inputs.R1
-    in_R2 = inputs.R2
-    fastqs = '-1 {0} -2 {1}'.format(inputs.R1, inputs.R2)
-elif flag == 'keep_R2':
-    fastqs = '-U {0}'.format(inputs.R2)
+if layout == "PE":
+    fastqs = "-1 {0} -2 {1}".format(r1, r2)
+elif layout == "keep_R2":
+    fastqs = "-U {0}".format(r2)
 else:
-    fastqs = '-U {0}'.format(inputs.R1)
+    fastqs = "-U {0}".format(r1)
 
 # Look up strand
-flag2 = get_flag(inputs.strand)
+strand = get_flag(snakemake.input.strand)
 
-if (flag == 'PE') & (flag2 == 'first_strand'):
-    strand = '--rna-strandness FR'
-elif (flag == 'PE') & (flag2 == 'second_strand'):
-    strand = '--rna-strandness RF'
-elif flag2 == 'first_strand':
-    strand = '--rna-strandness F'
-elif flag2 == 'second_strand':
-    strand = '--rna-strandness R'
+if (layout == "PE") & (strand == "first_strand"):
+    strand_param = "--rna-strandness FR"
+elif (layout == "PE") & (strand == "second_strand"):
+    strand_param = "--rna-strandness RF"
+elif strand == "first_strand":
+    strand_param = "--rna-strandness F"
+elif strand == "second_strand":
+    strand_param = "--rna-strandness R"
 else:
-    strand = ''
+    strand_param = ""
 
 # Grab index information
 prefix = aligners.prefix_from_hisat2_index(snakemake.input.index)
 
 # Create temporary files to store intermediates. Will use $TMDPIR if set.
-sam = NamedTemporaryFile(suffix='.sam', delete=False).name
-bam = NamedTemporaryFile(suffix='.bam', delete=False).name
-sort_bam = NamedTemporaryFile(suffix='.sort.bam', delete=False).name
+sam = NamedTemporaryFile(suffix=".sam", delete=False).name
+bam = NamedTemporaryFile(suffix=".bam", delete=False).name
+sort_bam = NamedTemporaryFile(suffix=".sort.bam", delete=False).name
 
 shell(
     "hisat2 "
@@ -59,18 +55,13 @@ shell(
     "{fastqs} "
     "--threads {snakemake.threads} "
     "{hisat2_extra} "
-    "{strand} "
+    "{strand_param} "
     "-S {sam} "
     "{log}"
 )
 
 # hisat2 outputs SAM format so we convert to BAM here.
-shell(
-    "samtools view -Sb "
-    "{samtools_view_extra} "
-    "{sam} "
-    "> {bam} && rm {sam}"
-)
+shell("samtools view -Sb {samtools_view_extra} {sam} > {bam} && rm {sam}")
 
 # sort the BAM and clean up
 shell(
@@ -79,8 +70,8 @@ shell(
     "{samtools_sort_extra} "
     "-O BAM "
     "{bam} "
-    "&& cp {sort_bam} {outputs.bam} "
+    "&& cp {sort_bam} {snakemake.output.bam} "
     "&& rm {bam} "
     "&& rm {sort_bam} "
-    "&& samtools index {outputs.bam} "
+    "&& samtools index {snakemake.output.bam} "
 )
