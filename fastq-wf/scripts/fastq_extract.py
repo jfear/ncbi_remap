c923977b4626afb84a8d3b033b42d35205d734cc
Justin Fear <justin.m.fear@gmail.com>
2020-02-21 05:57:37 -0500

    Adds staging step to fastq_extract.

--- fastq-wf/scripts/fastq_extract.py
+++ fastq-wf/scripts/fastq_extract.py
@@ -11,7 +11,8 @@ TMPDIR = os.getenv("TMPDIR", "/tmp")
 
 
 def main():
-    extract_fastq()
+    staged_data = stage_data()
+    extract_fastq(staged_data)
 
     if Path(TMPDIR, f"{snakemake.wildcards.srr}.sra.fastq").exists():
         # Single ended
@@ -23,9 +24,17 @@ def main():
         shutil.move(f"{TMPDIR}/{snakemake.wildcards.srr}.sra_2.fastq", snakemake.output.r2)
 
 
-def extract_fastq():
+def stage_data() -> str:
+    """Copy data over to local scratch space."""
+    data_pth = Path(snakemake.input[0])
+    tmp_pth = Path(TMPDIR, data_pth.name)
+    shutil.copy2(data_pth, tmp_pth)
+    return tmp_pth.as_posix()
+
+
+def extract_fastq(sra_file):
     shell(
-        f"fasterq-dump {snakemake.input[0]} "
+        f"fasterq-dump {sra_file} "
         "--split-3 "
         "--skip-technical "
         "--min-read-len 20 "
