d88bee5cbe85089beb6069ef786a5586adfed164
Justin M. Fear <justin.m.fear@gmail.com>
2020-03-04 09:08:42 -0500

    Modularizes layout.

--- agg-prealn-wf/scripts/layout.py
+++ agg-prealn-wf/scripts/layout.py
@@ -2,54 +2,36 @@
 import os
 from pathlib import Path
 import csv
+from ncbi_remap.aggregation import pull_processed_samples, touch_agg_file
 from ncbi_remap.snakemake import get_flag
-
-HEADER = ["srx", "srr", "layout"]
 
 
 def main():
-    queue = snakemake.params.queue
-    data_store = snakemake.params.data_store
+    header = ["srr", "layout"]
+    touch_agg_file(snakemake.params[0], header)
+    complete_srrs = pull_processed_samples(snakemake.params[0])
 
-    complete_srrs = check_srrs(data_store)
-
-    with open(data_store, "a") as file_out:
-        writer = csv.writer(file_out, delimiter="\t")
+    with open(snakemake.params[0], "a", newline="") as csvfile:
+        writer = csv.writer(csvfile)
         for file_name in snakemake.input:
             srr = Path(file_name).parent.stem
             if srr in complete_srrs:
                 continue
-            writer.writerow([queue.get_srx(srr), srr, get_flag(file_name)])
+            writer.writerow([srr, get_flag(file_name)])
 
     Path(snakemake.output[0]).touch()
-
-
-def check_srrs(file_name: str) -> set:
-    if Path(file_name).exists():
-        with open(file_name, "r") as file_in:
-            reader = csv.reader(file_in, delimiter="\t")
-            _header = next(reader)
-            return {row[1] for row in reader}
-
-    with open(file_name, "w") as file_out:
-        writer = csv.writer(file_out, delimiter="\t")
-        writer.writerow(HEADER)
-    return set()
 
 
 if __name__ == "__main__":
     if os.getenv("SNAKE_DEBUG"):
         from ncbi_remap.debug import snakemake_debug
-        from ncbi_remap.queue import Queue
-
-        queue = Queue(targets="../../output/prealn-wf/done", srx2srr="../../output/srx2srr.csv")
 
         snakemake = snakemake_debug(
             input=[
                 "../../output/fastq-wf/fastq_info/SRR031714/LAYOUT",
                 "../../output/fastq-wf/fastq_info/SRR031715/LAYOUT",
             ],
-            params=dict(queue=queue, data_store="../../output/agg-prealn-wf/layout.tsv"),
+            params="../../output/agg-prealn-wf/layout.csv",
         )
 
     main()
