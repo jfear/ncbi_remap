99ed0b63d97c10355b616905cfd2f73d7b1b5755
Justin M. Fear <justin.m.fear@gmail.com>
2020-03-16 14:15:42 -0400

    Adds file expansion to the queue.

--- src/tests/test_queue.py
+++ src/tests/test_queue.py
@@ -4,6 +4,7 @@ import pytest
 
 from ncbi_remap.queue import Queue
 
+TEST_DATA = Path(Path(__file__).parent / "../test_data/srx2srr.csv").absolute()
 
 @pytest.fixture(scope="session")
 def srx_folder(tmpdir_factory):
@@ -50,79 +51,90 @@ def srr_pkl(tmpdir_factory):
 
 
 def test_base_queue():
-    queue = Queue()
+    queue = Queue(srx2srr=TEST_DATA)
     assert queue.n_srxs > 0
     assert queue.n_srrs > 0
 
 
 def test_srx_target_folder(srx_folder):
-    queue = Queue(targets=srx_folder)
+    queue = Queue(targets=srx_folder, srx2srr=TEST_DATA)
     assert queue.n_srxs == 3
     assert queue.n_srrs > 0
 
 
 def test_srx_target_list(srx_list):
-    queue = Queue(targets=srx_list)
+    queue = Queue(targets=srx_list, srx2srr=TEST_DATA)
     assert queue.n_srxs == 3
     assert queue.n_srrs > 0
 
 
 def test_srx_target_pkl(srx_pkl):
-    queue = Queue(targets=srx_pkl)
+    queue = Queue(targets=srx_pkl, srx2srr=TEST_DATA)
     assert queue.n_srxs == 3
     assert queue.n_srrs > 0
 
 
 def test_srr_target_folder(srr_folder):
-    queue = Queue(targets=srr_folder)
+    queue = Queue(targets=srr_folder, srx2srr=TEST_DATA)
     assert queue.n_srxs > 0
     assert queue.n_srrs == 3
 
 
 def test_srr_target_list(srr_list):
-    queue = Queue(targets=srr_list)
+    queue = Queue(targets=srr_list, srx2srr=TEST_DATA)
     assert queue.n_srxs > 0
     assert queue.n_srrs == 3
 
 
 def test_srr_target_pkl(srr_pkl):
-    queue = Queue(targets=srr_pkl)
+    queue = Queue(targets=srr_pkl, srx2srr=TEST_DATA)
     assert queue.n_srxs == 3
     assert queue.n_srrs > 0
 
 
 def test_srx_problems_folder(srx_folder):
-    queue = Queue(problems=srx_folder)
+    queue = Queue(problems=srx_folder, srx2srr=TEST_DATA)
     assert queue.n_srxs == len(queue.srx2srr.srx.unique()) - 3
 
 
 def test_srr_problems_folder(srr_folder):
-    queue = Queue(problems=srr_folder)
+    queue = Queue(problems=srr_folder, srx2srr=TEST_DATA)
     assert queue.n_srrs == len(queue.srx2srr.srr.unique()) - 3
 
 
 def test_srx_subset_folder(srx_folder):
-    queue = Queue(subset=srx_folder)
+    queue = Queue(subset=srx_folder, srx2srr=TEST_DATA)
     assert queue.n_srxs == 3
 
 
 def test_srr_subset_folder(srr_folder):
-    queue = Queue(subset=srr_folder)
+    queue = Queue(subset=srr_folder, srx2srr=TEST_DATA)
     assert queue.n_srrs == 3
 
 
 def test_srx_sample_table():
-    queue = Queue(targets=["DRX000774", "DRX000775", "SRX000437"], size=2)
+    queue = Queue(targets=["DRX000774", "DRX000775", "SRX1006261"], size=2, srx2srr=TEST_DATA)
     assert sorted(queue.srxs) == sorted(queue.sample_table.srx.unique().tolist())
 
 
 def test_get_srx():
-    queue = Queue()
+    queue = Queue(srx2srr=TEST_DATA)
     srx = queue.get_srx("DRR001177")
     assert srx == "DRX000774"
 
 
 def test_get_srrs():
-    queue = Queue()
+    queue = Queue(srx2srr=TEST_DATA)
     srrs = queue.get_srrs("DRX000774")
     assert srrs == ["DRR001177"]
+
+def test_srr_expand():
+    srx = "DRX000774"
+    srr = "DRR001177"
+    queue = Queue(srx2srr=TEST_DATA)
+    assert queue.expand("test/{srx}/{srr}", srr) == f"test/{srx}/{srr}"
+
+def test_srx_expand():
+    srx = "SRX1006261"
+    queue = Queue(srx2srr=TEST_DATA)
+    assert len(queue.expand("test/{srx}/{srr}", srx)) == 4
