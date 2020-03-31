68d0d5ce6fac2f70f6bd5daec90e2d6d04f458c4
Justin M. Fear <justin.m.fear@gmail.com>
2020-03-31 12:46:21 -0400

    Oops this is really what aggs gbc.

--- library_strategy-wf/scripts/build_prealn_feature_set.py
+++ library_strategy-wf/scripts/build_prealn_feature_set.py
@@ -18,11 +18,12 @@ Features include:
     * PERCENT_DUPLICATION
 * Fastq Screen
     * Percent reads mapping to rRNA.
-* FeatureCounts 
+* FeatureCounts
     * Number of reads mapping to junction
 """
 import os
 from pathlib import Path
+from multiprocessing import Pool
 
 import pandas as pd
 
@@ -73,108 +74,7 @@ FEATURE_AGG = {
     "median_cv_coverage": "mean",
     # "median_5prime_bias": "mean",
     # "median_3prime_bias": "mean",
-    "median_5prime_to_3prime_bias": "mean",
-    "pos_0": "mean",
-    "pos_1": "mean",
-    "pos_2": "mean",
-    "pos_3": "mean",
-    "pos_4": "mean",
-    "pos_5": "mean",
-    "pos_6": "mean",
-    "pos_7": "mean",
-    "pos_8": "mean",
-    "pos_9": "mean",
-    "pos_10": "mean",
-    "pos_11": "mean",
-    "pos_12": "mean",
-    "pos_13": "mean",
-    "pos_14": "mean",
-    "pos_15": "mean",
-    "pos_16": "mean",
-    "pos_17": "mean",
-    "pos_18": "mean",
-    "pos_19": "mean",
-    "pos_20": "mean",
-    "pos_21": "mean",
-    "pos_22": "mean",
-    "pos_23": "mean",
-    "pos_24": "mean",
-    "pos_25": "mean",
-    "pos_26": "mean",
-    "pos_27": "mean",
-    "pos_28": "mean",
-    "pos_29": "mean",
-    "pos_30": "mean",
-    "pos_31": "mean",
-    "pos_32": "mean",
-    "pos_33": "mean",
-    "pos_34": "mean",
-    "pos_35": "mean",
-    "pos_36": "mean",
-    "pos_37": "mean",
-    "pos_38": "mean",
-    "pos_39": "mean",
-    "pos_40": "mean",
-    "pos_41": "mean",
-    "pos_42": "mean",
-    "pos_43": "mean",
-    "pos_44": "mean",
-    "pos_45": "mean",
-    "pos_46": "mean",
-    "pos_47": "mean",
-    "pos_48": "mean",
-    "pos_49": "mean",
-    "pos_50": "mean",
-    "pos_51": "mean",
-    "pos_52": "mean",
-    "pos_53": "mean",
-    "pos_54": "mean",
-    "pos_55": "mean",
-    "pos_56": "mean",
-    "pos_57": "mean",
-    "pos_58": "mean",
-    "pos_59": "mean",
-    "pos_60": "mean",
-    "pos_61": "mean",
-    "pos_62": "mean",
-    "pos_63": "mean",
-    "pos_64": "mean",
-    "pos_65": "mean",
-    "pos_66": "mean",
-    "pos_67": "mean",
-    "pos_68": "mean",
-    "pos_69": "mean",
-    "pos_70": "mean",
-    "pos_71": "mean",
-    "pos_72": "mean",
-    "pos_73": "mean",
-    "pos_74": "mean",
-    "pos_75": "mean",
-    "pos_76": "mean",
-    "pos_77": "mean",
-    "pos_78": "mean",
-    "pos_79": "mean",
-    "pos_80": "mean",
-    "pos_81": "mean",
-    "pos_82": "mean",
-    "pos_83": "mean",
-    "pos_84": "mean",
-    "pos_85": "mean",
-    "pos_86": "mean",
-    "pos_87": "mean",
-    "pos_88": "mean",
-    "pos_89": "mean",
-    "pos_90": "mean",
-    "pos_91": "mean",
-    "pos_92": "mean",
-    "pos_93": "mean",
-    "pos_94": "mean",
-    "pos_95": "mean",
-    "pos_96": "mean",
-    "pos_97": "mean",
-    "pos_98": "mean",
-    "pos_99": "mean",
-    "pos_100": "mean",
+    # "median_5prime_to_3prime_bias": "mean",
     "unpaired_reads_examined": "sum",
     "read_pairs_examined": "sum",
     "unpaired_read_duplicates": "sum",
@@ -185,23 +85,47 @@ FEATURE_AGG = {
     "percent_genes_on": "mean",
     "number_junction_reads": "sum",
     "number_junctions_on": "sum",
+    "gene_body_five_prime": "mean",
+    "gene_body_middle": "mean",
+    "gene_body_three_prime": "mean",
 }
 
 
 def main():
+    pool = Pool(snakemake.threads)
+
     srx2srr = pd.read_csv(snakemake.input[0], index_col="srr")
     done = {pth.stem for pth in Path(snakemake.params.done).iterdir()}
     df = pd.concat(
-        [
-            pd.read_parquet(v, engine="pyarrow", use_threads=True)
-            for k, v in snakemake.params.items()
-            if k != "done"
-        ],
+        pool.map(pd.read_parquet, [v for k, v in snakemake.params.items() if k != "done"]),
         axis=1,
         sort=False,
     ).reindex(done)
 
-    df.join(srx2srr).groupby("srx").agg(FEATURE_AGG).to_parquet(snakemake.output[0])
+    (
+        df.join(srx2srr)
+        .pipe(aggregate_gene_body_coverage)
+        .groupby("srx")
+        .agg(FEATURE_AGG)
+        .to_parquet(snakemake.output[0])
+    )
+
+
+def aggregate_gene_body_coverage(df):
+    """Sum gene body coverage to tertile.
+
+    GBC is reported as a centile, with positions next to each other being
+    highly correlated. For machine learning, I am aggregating these features
+    into a tertiles (i.e., 5', middle, 3').
+    """
+    cols = [f"pos_{i}" for i in range(101)]
+    five_prime, middle, three_prime = cols[:33], cols[33:68], cols[68:]
+    return (
+        df.assign(gene_body_five_prime=lambda x: x[five_prime].sum(axis=1))
+        .assign(gene_body_middle=lambda x: x[middle].sum(axis=1))
+        .assign(gene_body_three_prime=lambda x: x[three_prime].sum(axis=1))
+        .drop(cols, axis=1)
+    )
 
 
 if __name__ == "__main__":
@@ -213,6 +137,7 @@ if __name__ == "__main__":
 
         snakemake = snakemake_debug(
             input="../../output/srx2srr_full.csv",
+            threads=8,
             params=dict(
                 libsize="../../output/fastq-wf/libsize",
                 layout="../../output/fastq-wf/layout",
