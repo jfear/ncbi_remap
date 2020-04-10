ea14784fa38c0080ad50af7c28b8c95e55c6ba09
Justin M. Fear <justin.m.fear@gmail.com>
2020-04-10 14:30:19 -0400

    Restructures outlier detection.

--- library_strategy-wf/scripts/outlier_detection.py
+++ library_strategy-wf/scripts/outlier_detection.py
@@ -11,75 +11,74 @@ from sklearn.ensemble import IsolationForest
 import shap
 import joblib
 
-
 RANDOM_STATE = np.random.RandomState(42)
 
 
 def main():
-    labels_from_sra = pd.read_parquet(snakemake.input.labels)
-    strategy2srx = {k: v.index for k, v in labels_from_sra.groupby("library_strategy")}
+    # Build RNA-Seq anomaly detection model
+    rnaseq_features = pd.read_parquet(snakemake.input.rnaseq)
+    X_train, X_test = train_test_split(rnaseq_features, random_state=RANDOM_STATE)
+    iso = IsolationForest(n_estimators=100, random_state=RANDOM_STATE).fit(X_train)
 
-    features = pd.read_parquet(snakemake.input.features)
+    joblib.dump(iso, snakemake.output.iso)
+    joblib.dump(X_train, snakemake.output.train)
+    joblib.dump(X_test, snakemake.output.test)
 
-    rnaseq_features = features.reindex(strategy2srx["RNA-Seq"]).dropna(how="all")
-    rnaseq_train, rnaseq_test = train_test_split(rnaseq_features, random_state=RANDOM_STATE)
-    model = IsolationForest(n_estimators=100, random_state=RANDOM_STATE).fit(rnaseq_train)
+    # Detect outliers in Train and Test and make sure similar proportions.
+    pred_train = np.mean(np.where(iso.predict(X_train) == -1, True, False))
+    pred_test = np.mean(np.where(iso.predict(X_test) == -1, True, False))
+    assert np.isclose(pred_test, pred_train, atol=0.01)
 
-    # Detect outliers and save out
-    estimate_feature_importance(model, rnaseq_features)
-    outliers = save_outliers(model, rnaseq_features)
-    save_list_of_good_rnaseq(outliers)
-
-    # Validate model using ChIP-Seq and WGS mixin
-    chip_features = features.reindex(strategy2srx["ChIP-Seq"]).dropna(how="all")
-    wgs_features = features.reindex(strategy2srx["WGS"]).dropna(how="all")
+    # Test model by mixing in other types of library strategy
+    est_features = pd.read_parquet(snakemake.input.est)
+    wgs_features = pd.read_parquet(snakemake.input.wgs)
+    chip_features = pd.read_parquet(snakemake.input.chip)
     pd.concat(
         [
-            mixin_contamination(rnaseq_test, chip_features, "ChIP-Seq", model),
-            mixin_contamination(rnaseq_test, wgs_features, "WGS", model),
+            mixin_contamination(iso, X_test, est_features, "EST"),
+            mixin_contamination(iso, X_test, wgs_features, "WGS"),
+            mixin_contamination(iso, X_test, chip_features, "ChIP-Seq"),
         ]
     ).to_parquet(snakemake.output.mixin)
 
-
-def estimate_feature_importance(model, rnaseq_features):
-    explainer = shap.TreeExplainer(model)
-    shap_values = explainer.shap_values(rnaseq_features)
-    joblib.dump(explainer, snakemake.output.shap_model)
-    joblib.dump(rnaseq_features, snakemake.output.shap_features)
-    joblib.dump(shap_values, snakemake.output.shap_values)
-
-
-def save_outliers(model, rnaseq_features):
-    outliers = pd.DataFrame(
-        np.where(model.predict(rnaseq_features) == -1, True, False),
-        index=rnaseq_features.index,
-        columns=["rnaseq_outliers"],
-    )
+    # Detect rnaseq outliers and save out
+    outliers = df_outliers(iso, rnaseq_features)
     outliers.to_parquet(snakemake.output.outliers)
-    return outliers
+
+    # Save a list of good rnaseq samples
+    inliers = outliers[~outliers.squeeze()].index.tolist()
+    joblib.dump(inliers, snakemake.output.rnaseq_inliers)
 
 
-def save_list_of_good_rnaseq(outliers):
-    rnaseq = outliers[~outliers.squeeze()].index.tolist()
-    joblib.dump(rnaseq, snakemake.output.rnaseq_samples)
+def mixin_contamination(model, rnaseq: pd.DataFrame, other: pd.DataFrame, source: str):
+    """Mix in different proportions of 'Other' samples.
 
-
-def mixin_contamination(rnaseq: pd.DataFrame, other: pd.DataFrame, source, model):
+    Mixes different proportions (0 - 1) of `other` and measures proportion of
+    outliers.
+    """
     n = min(rnaseq.shape[0], other.shape[0])
     res = []
-    for rna_frac in np.arange(0.1, 1.1, step=0.1)[::-1]:
-        _rna_frac = np.round(rna_frac, 2)
-        _other_frac = 1 - _rna_frac
+    for rna_frac in [1.0, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1, 0.0]:
+        other_frac = 1 - rna_frac
         mixture = pd.concat(
-            [rnaseq.sample(n=int(n * _rna_frac)), other.sample(n=int(n * _other_frac))]
+            [rnaseq.sample(n=int(n * rna_frac)), other.sample(n=int(n * other_frac))]
         )
         prediction = model.predict(mixture)
         prop_outliers = np.mean(np.where(prediction == -1, 1, 0))
-        res.append((_other_frac, prop_outliers, source))
+        res.append((other_frac, prop_outliers, source))
 
     return pd.DataFrame(
         res, columns=["Prop Contamination", "Prop Outliers", "Contamination Source"]
     )
+
+
+def df_outliers(model, features: pd.DataFrame) -> pd.DataFrame:
+    outliers = pd.DataFrame(
+        np.where(model.predict(features) == -1, True, False),
+        index=features.index,
+        columns=["rnaseq_outliers"],
+    )
+    return outliers
 
 
 if __name__ == "__main__":
@@ -88,10 +87,11 @@ if __name__ == "__main__":
 
         snakemake = MockSnake(
             input=dict(
-                features="../../output/library_strategy-wf/scaled_prealn_feature_set.parquet",
-                labels="../../output/library_strategy-wf/sra_strategy_selection.parquet",
+                rnaseq="../../output/library_strategy-wf/rnaseq_features.parquet",
+                est="../../output/library_strategy-wf/est_features.parquet",
+                wgs="../../output/library_strategy-wf/wgs_features.parquet",
+                chip="../../output/library_strategy-wf/chip_features.parquet",
             ),
-            output=dict(shap_model="", shap_values="", outliers="", mixin=""),
         )
 
     main()
