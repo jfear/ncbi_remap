72ae2b236da09f713ae97da8a8ba8e615a8b23a9
Justin Fear <justin.m.fear@gmail.com>
2020-05-19 13:53:18 -0400

    Tweaks src direcotry to work in container.

--- rnaseq-wf/scripts/parse_junction_counts.py
+++ rnaseq-wf/scripts/parse_junction_counts.py
@@ -1,6 +1,7 @@
 import sys
+import os
 
-sys.path.insert(0, "../../src")
+sys.path.insert(0, os.getenv("SRC_PATH", "../src"))
 from ncbi_remap.parser import parse_featureCounts_jcounts
 
 df = parse_featureCounts_jcounts(snakemake.input[0], snakemake.wildcards.srx)
