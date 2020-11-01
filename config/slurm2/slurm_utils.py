f53afebf6cac80eb82112f4c2fed8de81ffe21cc
Justin Fear <justin.m.fear@gmail.com>
2020-11-01 11:45:51 -0500

    bump.

--- config/slurm2/slurm_utils.py
+++ config/slurm2/slurm_utils.py
@@ -77,12 +77,11 @@ def format_wildcards(string, job_properties):
 
     _variables = dict()
     _variables.update(
-        dict(
-            params=job._format_params,
-            wildcards=job._format_wildcards,
-            rule=job.rule
-        )
+        dict(params=job._format_params, wildcards=job._format_wildcards)
     )
+    if hasattr(job, "rule"):
+        _variables.update(dict(rule=job.rule))
+
     try:
         return format(string, **_variables)
     except NameError as ex:
